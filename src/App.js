eb6d5f89375d20dd4acc9f1117e4f24772077633
github username <github email address>
2019-07-31 17:40:14 -0400

    installed redux, react-redux, and redux-thunk. set up imports in index.js file.

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import React, {Component} from 'react';
 
 class App extends Component{
 
-  componentDidMount(){
-    fetch('http://localhost:3000/api/v1/programs', {
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'}
-      }
-     )
-    .then(res => res.json())
-    .then(json => console.log(json))
-
-  }
+  // HAD HERE TO TEST BACKEND
+  // componentDidMount(){
+  //   fetch('http://localhost:3000/api/v1/programs', {
+  //     method: 'GET',
+  //     headers: {
+  //       'Accept': 'application/json',
+  //       'Content-Type': 'application/json'}
+  //     }
+  //    )
+  //   .then(res => res.json())
+  //   .then(json => console.log(json))
+  //
+  // }
 
   render(){
     return (
--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore, applyMiddleware} from 'redux'
+import {Provider} from 'react-redux'
+import thunk from 'redux-thunk'
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// Learn more about service workers: https://bit.ly/CRA-PWA
-// unregister() to register() below. Note this comes with some pitfalls. You would also need to import the serviceworker file. 
-// serviceWorker.unregister();
