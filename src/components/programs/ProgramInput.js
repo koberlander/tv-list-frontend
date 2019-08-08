76ee73aa4cf44d93515dcc6d9e84ffa9b526ea4c
github username <github email address>
2019-08-08 14:06:44 -0400

    started form input size in css, set up state and handle Change on form inputs

--- src/components/programs/ProgramInput.js
+++ src/components/programs/ProgramInput.js
@@ -1,18 +1,28 @@
 import React from 'react'
 
 class ProgramInput extends React.Component {
-  constructor(){
-    super()
 
-    this.state = {
-      text: ''
+  state = {
+      url: '',
+      name: '',
+      network: '',
+      image: '',
+      watchlist: false
     }
-}
+
 
   handleChange = event => {
     this.setState({
       [event.target.name]: event.target.value
     })
+  }
+
+  handleCheckbox = event => {
+    if (event.target.value === !false) {
+
+        //check labs, then dad jokes for this logic
+
+    }
   }
 
   handleSubmit = event => {
@@ -29,45 +39,59 @@ class ProgramInput extends React.Component {
 
   render(){
     return(
-      <form >
-          <ul className='form-style-1'>
-            <li>
-              <label>Main TV Show Page Link</label>
+        <div className='form div'>
+          <form className="ui form">
+            <div className="six width field">
+              <label>Main Show Info Link</label>
               <input
-                type='email'
-                name='field1'
-                className='field-long'
+                type='text'
+                name='url'
+                placeholder="https://example.com"
+                value={this.state.url}
                 onChange={this.handleChange}/>
-            </li>
-            <li>
-              <label>Name</label>
+            </div>
+            <div className="six width field">
+              <label>Show Name</label>
               <input
-              type='name'
-              name='field2'
-              className='field-long'
-              onChange={this.handleChange} />
-            </li>
-          <li>
-            <label>Network</label>
-            <input
-            type='network'
-            name='field3'
-            className='field-long'
-            onChange={this.handleChange} />
-          </li>
-          <li>
-            <label>Image Url</label>
-            <input
-            type='email'
-            name='field4'
-            className='field-long'
-            onChange={this.handleChange} />
-          </li>
-          <li>
-            <input type='submit' value='Submit' />
-          </li>
-        </ul>
-      </form>
+                type='text'
+                name='name'
+                placeholder="Your show here"
+                value={this.state.name}
+                onChange={this.handleChange}/>
+            </div>
+            <div className="six width field">
+              <label>Network Name</label>
+              <input
+                type='text'
+                name='network'
+                placeholder="Netflix, Hulu, HBO, Showtime..."
+                value={this.state.network}
+                onChange={this.handleChange}/>
+            </div>
+            <div className="six width field">
+              <label>Image URL</label>
+              <input
+                type='text'
+                name='image'
+                placeholder="http://image.png"
+                value={this.state.image}
+                onChange={this.handleChange}/>
+            </div>
+            <div className="six width field">
+              <div className="ui checkbox">
+                  <input
+                  type="checkbox"
+                  className="hidden"
+                  readOnly=""
+                  tabIndex="0"
+                  value={this.state.watchlist}
+                  onChange={this.handleCheckbox}/>
+                <label>Add to Watchlist</label>
+              </div>
+            </div>
+            <button type="submit" className="ui button">Submit</button>
+          </form>
+        </div>
     )
   }
 }
