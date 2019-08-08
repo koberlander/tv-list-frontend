4253a42975692ec1c48f48c55887c1ac99075d0e
github username <github email address>
2019-08-08 12:23:13 -0400

    fixed background and text color for Add New button

--- src/components/programs/Programs.js
+++ src/components/programs/Programs.js
@@ -5,23 +5,26 @@ import Program from './Program'
 const Programs = (props) => {
 
   return(
-    <div className='ui six column grid'>
-        <div className='row'>
-          {props.programs.map((program) => <Program
-            key={program.id}
-            url={program.url}
-            name={program.name}
-            network={program.network}
-            image={program.image}
-            watchlist={program.watchlist}
-            program={program}/>)}
+      <div>
+        <div className='add button div'>
+          <button className='ui right aligned button'>Add New</button>
         </div>
-    </div>
+
+        <div className='ui six column grid'>
+            <div className='row'>
+              {props.programs.map((program) => <Program
+                key={program.id}
+                url={program.url}
+                name={program.name}
+                network={program.network}
+                image={program.image}
+                watchlist={program.watchlist}
+                program={program}/>)}
+            </div>
+        </div>
+
+      </div>
   )
 }
 
 export default Programs
-
-// <div>
-// <button className='ui right floated button'>Add</button>
-// </div>,
