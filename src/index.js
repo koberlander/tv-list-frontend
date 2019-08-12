d1ebdef4d348a7b6bb848b43aa7381244b7b1da4
github username <github email address>
2019-08-12 10:33:58 -0400

    updated comment on index.js

--- src/index.js
+++ src/index.js
@@ -8,13 +8,12 @@ import App from './App';
 import programReducer from './reducers/programReducer'
 
 //Compose allows me to combine more than one middleware.
-//More info: https://redux.js.org/api/compose
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION__COMPOSE__ || compose
 
 //createStore takes in my reducer and the composeEnhancers function we made above allows me to use dev tools AND apply thunk. In short, I made deeply nested functions and saved them to the store variable. Sneaky!
 const store = createStore(programReducer, composeEnhancers(applyMiddleware(thunk)))
 
-//we attach react router to access the views in our NavBar and use a switch statement so we only see one view at a time.
+//Still need to attach react router to access the views in Header/Menu and use a switch statement to see one view at a time.
 ReactDOM.render(
   <Provider store={store}>
     <App />
