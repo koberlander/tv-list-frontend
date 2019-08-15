9da07055c23c87ded36bbc14d96651e80af149c0
github username <github email address>
2019-08-15 16:00:42 -0400

    fixed /programs card rendering, started working on style.css issue

--- src/components/programs/Program.js
+++ src/components/programs/Program.js
@@ -5,7 +5,7 @@ import {Grid, Card, Image, Button} from 'semantic-ui-react'
 
 const Program = (props) => {
 
-console.log(props)
+console.log(props.program)
 
   let handleClick = (id) => {
     // debugger
@@ -13,19 +13,11 @@ console.log(props)
 
   }
 
-// May do this instead of ternary in the card fields
-//call props.showCard() in the return below instead of a ternary
-  // let showCard = () => {
-  //   if (program) {
-  //     return the card below
-  //   } else {
-  //     null (is the else even needed?)
-  //   }
-  // }
 
-/* To access a specific program/:id, 'routerProps' provides 'match.params.id'. So we can use our array of programs to access that specific id as shown below. */
+/* To access a specific program/:id, 'routerProps' provides 'match.params.id'. Can use it here to access the id needed by .mapping over my rograms array. */
 
-  let program = props.programs[props.match.params.id - 1]
+let program = props.program ? props.program : props.programs[props.match.params.id - 1]
+
   // console.log(program)
 
     //using ternary in return because I need programs array to be populated before rendering any of these cards.
@@ -42,37 +34,16 @@ console.log(props)
                 Notes
               </Card.Description>
             </Card.Content>
-            <Button.Group attached='bottom' >
-              <Button basic icon='pencil alternate' />
-              <Button basic icon='trash' />
-              <Button basic icon='heart' />
-            </Button.Group>
+            <Card.Content extra>
+              <div className='ui three buttons'>
+                <Button basic icon='pencil'></Button>
+                <Button basic icon='trash'></Button>
+                <Button basic icon='heart'></Button>  
+              </div>
+            </Card.Content>
           </Card>
         </Grid.Column>
     )
 }
 
 export default connect(null, {deleteProgram})(Program)
-
-// <div className='ui column' >
-//   <div className='ui card' onClick={(_) => this.handleClick(id)}>
-//   <div className="content">
-//   <a className="ui right corner label" >
-//   <i aria-hidden="true" className="trash alternate link icon"></i>
-//   </a>
-//   <img
-//   className="ui rounded image"
-//   src={image}
-//   alt='o nooo'
-//   />
-//   <div className="header">
-//   <a href={url}>{name}</a>
-//   </div>
-//   <div className="meta">{network}</div>
-//   </div>
-//   <div className="ui bottom attached button">
-//   <i className="add icon"></i>
-//   Watchlist
-//   </div>
-//   </div>
-// </div>
--- src/components/programs/Programs.js
+++ src/components/programs/Programs.js
@@ -1,12 +1,11 @@
 import React from 'react'
 import Program from './Program'
 import {Grid} from 'semantic-ui-react'
-import {Link} from 'react-router-dom'
+
 // Instead of passing all props in an arrow function below, I could just pass an object that contains my programs array. Here I chose to just use props because I'll be manipulating the data a lot.
 const Programs = (props) => {
-debugger
+// debugger
 
-// let id = props.program.id
 
   return(
       <div>
@@ -15,10 +14,8 @@ debugger
         </div>
 
         <Grid columns='six' divided='vertically'>
-          <Grid.Row to={`/program/${props.program.id}`}>
-              {props.programs.map((program, id) => <Program
-                key={id}
-                program={program}/>)}
+          <Grid.Row >
+              {props.programs.map((program) => <Program key={program.id} program={program}/>)}
           </Grid.Row>
         </Grid>
 
@@ -27,10 +24,3 @@ debugger
 }
 
 export default Programs
-//
-// <div className='ui six column grid'>
-//       {props.programs.map((program, id) => <Program
-//         key={id}
-//         program={program}/>)}
-//
-// </div>
