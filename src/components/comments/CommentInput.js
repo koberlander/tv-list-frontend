e037de97d9c64305197a1ea68c778ee06ff5810a
github username <github email address>
2019-09-01 10:10:03 -0400

    added avatar choices to form

--- src/components/comments/CommentInput.js
+++ src/components/comments/CommentInput.js
@@ -1,43 +1,61 @@
 import React from 'react'
-import {Form, Button} from 'semantic-ui-react'
+import {connect} from 'react-redux'
+import {Form, Button, Radio} from 'semantic-ui-react'
 
 const CommentInput = (props) => {
-//add hook to handle state
+//add hook to handle state?
     return(
+      <br>/<br>
+      <br></br>
       <Form >
-        <Form.Input />
-        <Form.TextArea />
+        <Form.Field>
+          Filler title text:
+        </Form.Field>
+
+        <Form.Field>
+          <Radio
+          label='Avatar 1'
+          name='radioGroup'
+          value='this'
+          />
+        </Form.Field>
+        <Form.Field>
+          <Radio
+          label='Avatar 2'
+          name='radioGroup'
+          value='that'
+          />
+        </Form.Field>
+        <Form.Field>
+          <Radio
+          label='Avatar 3'
+          name='radioGroup'
+          value='the other thing'
+          />
+        </Form.Field>
+
+        <Form.Input label='Author Name'/>
+
+        <Form.TextArea label='Comment'/>
+
         <Button content='Add Reply' labelPosition='left' icon='edit' primary />
       </Form>
     )
   }
 
+  // Choose an avatar: {/*<b>{this.state.value}</b>*/}
 
-  // <Form.Field label='Choose an avatar.'>
-  // <Radio
-  // label='Avatar 1'
-  // name='radioGroup'
-  // value='this'
+  //First avatar
   // checked={{/* this.state.value === 'this'*/}}
   // onChange={{/*this.handleChange*/}}
-  // />
-  // </Form.Field>
-  // <Form.Field>
-  // <Radio
-  // label='Avatar 2'
-  // name='radioGroup'
-  // value='that'
+
+  // 2nd avatar
+  // checked={{/* this.state.value === 'that'*/}}
+  // onChange={{/*this.handleChange*/}}
+
   // checked={{/* this.state.value === 'that'*/}}
   // onChange={{/*this.handleChange*/}}
   // />
   // </Form.Field>
-  // <Form.Field>
-  // <Radio
-  // label='Avatar 3'
-  // name='radioGroup'
-  // value='the other thing'
-  // checked={{/* this.state.value === 'that'*/}}
-  // onChange={{/*this.handleChange*/}}
-  // />
-  // </Form.Field>
-export default CommentInput
+
+export default connect(null)(CommentInput)
