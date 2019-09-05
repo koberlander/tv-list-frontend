0d75832249bd1543520ab1b1d54dc12249eef0da
github username <github email address>
2019-09-05 15:57:56 -0400

    added a programs directory and a comments directory to /actions

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import {Segment} from 'semantic-ui-react'
 
 const Footer = () => {
   return(
-      <div>
-        Powered by TVMAZE.com
-      </div>
+      <Segment as='div' className='footer' padded='very' textAlign='center' inverted>
+        Built by Kristin<br></br><br></br>
+        Powered by <a href='https://www.tvmaze.com/'>TVMAZE.com</a>
+      </Segment>
     )
 }
 export default Footer
