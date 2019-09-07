b8ec906c586b34fe56200563f704ad504991d19d
github username <github email address>
2019-09-07 09:28:15 -0400

    started on posting comment time in HH:MM

--- src/components/comments/CommentCard.js
+++ src/components/comments/CommentCard.js
@@ -3,6 +3,14 @@ import {Comment, Header} from 'semantic-ui-react'
 import CommentInput from './CommentInput'
 
 const CommentCard = (props) => {
+let magic = () => {
+  Date.now()
+}
+function prettyDate2(magic) {
+    let dateWithoutSecond = new Date();
+    dateWithoutSecond.toLocaleTimeString(navigator.language, {hour: '2-digit', minute:'2-digit'});
+}
+
 
     return(
       <Fragment>
@@ -15,11 +23,11 @@ const CommentCard = (props) => {
           <Comment.Content>
             <Comment.Author as='a'>Alex</Comment.Author>
             <Comment.Metadata>
-              <div>Today at 5:42PM</div>
+              <div>Today at {prettyDate2(magic)}</div>
             </Comment.Metadata>
             <Comment.Text>This show is great!</Comment.Text>
             <Comment.Actions>
-              
+
             </Comment.Actions>
           </Comment.Content>
         </Comment>
